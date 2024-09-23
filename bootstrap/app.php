5058bddc2a44c27c7ae1abf923464aebb75d9750
Shubham Raj <shubhambth0000@gmail.com>
2024-09-23 23:17:07 +0530

    Added 'livewire/update', 'super-admin/logout' to ignore for validateCsrfTokens.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: ['livewire/update', 'super-admin/logout']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
